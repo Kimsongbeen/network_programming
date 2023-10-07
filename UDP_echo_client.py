559e50b0f93f133edeaab382fcbdf626b92b190e
Kimsongbeen <118897208+Kimsongbeen@users.noreply.github.com>
2023-10-07 10:09:08 +0900

    1:1 채팅(UDP_echo)

--- UDP_echo_client.py
+++ UDP_echo_client.py
@@ -4,8 +4,8 @@ port = 2500
 
 #서버와 통신 유형의 소켓 생성
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-msg = input()  #HELLO UDP server
-sock.sendto(msg.encode(),('localhost', port))   #메세지 송신
-data, addr = sock.recvfrom(BUFFSIZE)    #메세지 수신
-print("Server says: ", data.decode())
+while True:
+    msg = input()  #HELLO UDP server
+    sock.sendto(msg.encode(),('localhost', port))   #메세지 송신
+    data, addr = sock.recvfrom(BUFFSIZE)    #메세지 수신
+    print("Server says: ", data.decode())
